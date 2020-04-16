b2e8af3139288acf18cb818bb36ec0207b56e42c
Vladislav <nvlad1@ukr.net>
2020-04-16 11:13:16 +0300

    Replaced AndroidManifest.xml to build.gradle file

--- step.sh
+++ step.sh
@@ -5,64 +5,64 @@ set -e
 
 #
 # Required parameters
-if [ -z "${manifest_file}" ] ; then
-  echo " [!] Missing required input: manifest_file"
+if [[ -z "${gradle_file}" ]] ; then
+  echo " [!] Missing required input: build.gradle file"
   exit 1
 fi
-if [ ! -f "${manifest_file}" ] ; then
-  echo " [!] File doesn't exist at specified AndroidManifest.xml path: ${manifest_file}"
+if [[ ! -f "${gradle_file}" ]] ; then
+  echo " [!] File doesn't exist at specified build.gradle file path: ${gradle_file}"
   exit 1
 fi
 
 # ---------------------
 # --- Configs:
 
-echo " (i) Provided Android Manifest path: ${manifest_file}"
+echo " (i) Provided build.gradle file path: ${gradle_file}"
 echo
 
 # ---------------------
 # --- Main
 
-VERSIONCODE=`grep versionCode ${manifest_file} | sed 's/.*versionCode="//;s/".*//'`
-VERSIONNAME=`grep versionName ${manifest_file} | sed 's/.*versionName\s*=\s*\"\([^\"]*\)\".*/\1/g'`
-PACKAGENAME=`grep package ${manifest_file} | sed 's/.*package\s*=\s*\"\([^\"]*\)\".*/\1/g'`
-MINSDKVERSION=`grep minSdkVersion ${manifest_file} | sed 's/.*minSdkVersion="//;s/".*//'`
-TARGETSDKVERSION=`grep targetSdkVersion ${manifest_file} | sed 's/.*targetSdkVersion="//;s/".*//'`
+VERSION_CODE=`grep versionCode ${gradle_file} | sed -e 's/[^.0-9]//g'`
+VERSION_NAME=`grep versionName ${gradle_file} | head -n 1 | sed -e 's/[^.0-9]//g'`
+APPLICATION_ID=`grep applicationId ${gradle_file} | head -n 1 | awk '{ print $2 }' | sed 's/"//g'`
+MIN_SDK_VERSION=`grep minSdkVersion ${gradle_file} | sed -e 's/[^.0-9]//g'`
+TARGET_SDK_VERSION=`grep targetSdkVersion ${gradle_file} | sed -e 's/[^.0-9]//g'`
 
-if [ -z "${VERSIONCODE}" ] ; then
+if [[ -z "${VERSION_CODE}" ]] ; then
   echo " [!] Could not find version code!"
   exit 1
 fi
 
-envman add --key AMI_VERSION_CODE --value "${VERSIONCODE}"
-echo " (i) Version Code: ${VERSIONCODE} -> Saved to \$AMI_VERSION_CODE environment variable."
+envman add --key GRADLE_VERSION_CODE --value "${VERSION_CODE}"
+echo " (i) Version Code: ${VERSION_CODE} -> Saved to \$GRADLE_VERSION_CODE environment variable."
 
-if [ -z "${VERSIONNAME}" ] ; then
+if [[ -z "${VERSION_NAME}" ]] ; then
   echo " [!] Could not find version name!"
   exit 1
 fi
 
-envman add --key AMI_VERSION_NAME --value "${VERSIONNAME}"
-echo " (i) Version Name: ${VERSIONNAME} -> Saved to \$AMI_VERSION_NAME environment variable."
+envman add --key GRADLE_VERSION_NAME --value "${VERSION_NAME}"
+echo " (i) Version Name: ${VERSION_NAME} -> Saved to \$GRADLE_VERSION_NAME environment variable."
 
-if [ -z "${PACKAGENAME}" ] ; then
-  echo " [!] Could not find package name!"
+if [[ -z "${APPLICATION_ID}" ]] ; then
+  echo " [!] Could not find application id!"
   exit 1
 fi
 
-envman add --key AMI_PACKAGE_NAME --value "${PACKAGENAME}"
-echo " (i) Package Name: ${PACKAGENAME} -> Saved to \$AMI_PACKAGE_NAME environment variable."
+envman add --key GRADLE_APPLICATION_ID --value "${APPLICATION_ID}"
+echo " (i) Application ID: ${APPLICATION_ID} -> Saved to \$GRADLE_APPLICATION_ID environment variable."
 
-if [ -z "${MINSDKVERSION}" ] ; then
-  echo " No minimum SDK version found in manifest"
+if [[ -z "${MIN_SDK_VERSION}" ]] ; then
+  echo " No minimum SDK version found in build.gradle file"
 else
-  envman add --key AMI_MIN_SDK_VERSION --value "${MINSDKVERSION}"
-  echo " (i) Minimum SDK version: ${MINSDKVERSION} -> Saved to \$AMI_MIN_SDK_VERSION environment variable."
+  envman add --key GRADLE_MIN_SDK_VERSION --value "${MIN_SDK_VERSION}"
+  echo " (i) Minimum SDK version: ${MIN_SDK_VERSION} -> Saved to \$GRADLE_MIN_SDK_VERSION environment variable."
 fi
 
-if [ -z "${TARGETSDKVERSION}" ] ; then
-  echo " No target SDK version found in manifest"
+if [[ -z "${TARGET_SDK_VERSION}" ]] ; then
+  echo " No target SDK version found in build.gradle file"
 else
-  envman add --key AMI_TARGET_SDK_VERSION --value "${TARGETSDKVERSION}"
-  echo " (i) Target SDK version: ${TARGETSDKVERSION} -> Saved to \$AMI_TARGET_SDK_VERSION environment variable."
+  envman add --key GRADLE_TARGET_SDK_VERSION --value "${TARGET_SDK_VERSION}"
+  echo " (i) Target SDK version: ${TARGET_SDK_VERSION} -> Saved to \$GRADLE_TARGET_SDK_VERSION environment variable."
 fi
